c48831ee0ae5131bdead27fb032d4ab7d83d6fad
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-11-05 16:38:58 -0300

    TG-22: Create button component

---
+++ src/Components/Shared/Button/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './button.module.css';
+
+const Button = ({ type = 'button', label, onClick, disabled, style, hidden, theme, icon }) => {
+  return (
+    <button
+      type={type}
+      className={`${theme && styles[theme]} ${style} ${disabled && styles.disabled}
+             ${hidden && styles.hidden} ${icon && styles.icon}`}
+      onClick={onClick}
+      disabled={disabled}
+      hidden={hidden}
+    >
+      {icon ? <img className={`${styles.icon} ${styles.image}`} src={icon} /> : label}
+    </button>
+  );
+};
+
+export default Button;
