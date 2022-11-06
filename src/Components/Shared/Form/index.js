256c1c4bd9675c6dafce44e29e9048d6cf1c2d89
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-11-06 19:42:52 -0300

    TG-26: Add second column option to form

--- src/Components/Shared/Form/index.js
+++ src/Components/Shared/Form/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import styles from './form.module.css';
+import Button from '../Button';
 
-const Form = ({ children, name, autoComplete, noValidate, onSubmit, legend, title }) => {
+const Form = ({
+  children,
+  name,
+  autoComplete,
+  noValidate = false,
+  onSubmit,
+  legend,
+  title,
+  secondColumnIndex
+}) => {
   return (
     <form
       className={styles.form}
@@ -11,10 +21,29 @@ const Form = ({ children, name, autoComplete, noValidate, onSubmit, legend, titl
       onSubmit={onSubmit}
     >
       {title ? (title = <h2 className={styles.title}>{title}</h2>) : null}
-      <fieldset className={styles.fieldset}>
-        {legend ? <legend className={styles.legend}>{legend}</legend> : null}
-        {children}
-      </fieldset>
+      <div className={secondColumnIndex ? styles.fieldsetColumns : null}>
+        <fieldset className={styles.fieldset}>
+          {legend ? (
+            <legend className={styles.legend}>{Array.isArray(legend) ? legend[0] : legend}</legend>
+          ) : null}
+          {children.slice(0, secondColumnIndex)}
+        </fieldset>
+        {secondColumnIndex ? (
+          <fieldset className={styles.fieldset}>
+            {Array.isArray(legend) && legend[1] ? (
+              <legend className={styles.legend}>{legend[1]}</legend>
+            ) : null}
+            {children.slice(secondColumnIndex)}
+          </fieldset>
+        ) : null}
+      </div>
+      <Button
+        type="submit"
+        label="Submit"
+        className={styles.submit}
+        disabled={noValidate}
+        theme={noValidate ? 'disabled' : 'primary'}
+      />
     </form>
   );
 };
