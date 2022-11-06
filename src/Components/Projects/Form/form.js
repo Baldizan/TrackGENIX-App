e6831cefc07765a8da835185e6a428fcf2b07285
Guido Gentile <guido.gentile.1995@gmail.com>
2022-11-06 02:21:42 -0300

    TG-5: Added confirmation messages for add and edit project

--- src/Components/Projects/Form/form.js
+++ src/Components/Projects/Form/form.js
@@ -63,6 +63,7 @@ const ProjectsForm = () => {
         .then((response) => response.json())
         .then(function (response) {
           if (!response.error) {
+            alert(`Project ${projectBody.name} updated successfully!`);
             window.location.assign('/projects');
           }
         });
@@ -77,6 +78,7 @@ const ProjectsForm = () => {
         .then((response) => response.json())
         .then(function (response) {
           if (!response.error) {
+            alert(`Project ${projectBody.name} created successfully!`);
             window.location.assign('/projects');
           }
         });
