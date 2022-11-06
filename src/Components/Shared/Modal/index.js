bcbdbcfe858470085d399333cb7c13a14057ab5f
Guido Gentile <guido.gentile.1995@gmail.com>
2022-11-06 20:24:43 -0300

    TG-24: Given styles for confirm delete modal, added button shared components

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import styles from './modal.module.css';
+import Button from '../Button';
 
 const Modal = ({ content, contentMessage, heading, setModalDisplay, reqFunction }) => {
   return (
-    <div className={styles.modal}>
-      <header className={styles.header}>
-        <h2 className={styles.heading}>{heading}</h2>
-        <button className={styles.topclosebutton} onClick={() => setModalDisplay()}>
-          X
-        </button>
-      </header>
-      <div className={styles.content}>
-        {content ?? null}
-        {contentMessage && !content ? <p className={styles.content}>{contentMessage}</p> : null}
-        {contentMessage && !content ? (
-          <div>
-            <button
-              type="submit"
-              className={styles.confirmbutton}
-              onClick={() => {
-                reqFunction();
-              }}
-            >
-              Confirm
-            </button>
-            <button className={styles.bottomclose__button} onClick={() => setModalDisplay()}>
-              Cancel
-            </button>
-          </div>
-        ) : null}
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <header className={styles.header}>
+          <h2 className={styles.heading}>{heading}</h2>
+          <button className={styles.topCloseButton} onClick={() => setModalDisplay(false)}>
+            X
+          </button>
+        </header>
+        <div className={styles.content}>
+          {content ?? null}
+          {contentMessage && !content ? <p className={styles.content}>{contentMessage}</p> : null}
+          {contentMessage && !content ? (
+            <div className={styles.buttons}>
+              <Button
+                label={'Cancel'}
+                theme={'primary'}
+                type="submit"
+                onClick={() => {
+                  reqFunction();
+                }}
+              />
+              <Button
+                label={'Confirm'}
+                theme={'tertiary'}
+                type="submit"
+                onClick={() => {
+                  reqFunction();
+                }}
+              />
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
