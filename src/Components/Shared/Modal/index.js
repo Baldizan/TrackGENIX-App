7313607838b508e8480dc0faaff22a1cd7d6015e
Guido Gentile <guido.gentile.1995@gmail.com>
2022-11-08 00:18:16 -0300

    TG-24: Added image for cross close button, finished modals with styles

--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -2,27 +2,35 @@ import React from 'react';
 import styles from './modal.module.css';
 import Button from '../Button';
 
-const Modal = ({ content, contentMessage, heading, setModalDisplay, reqFunction, theme }) => {
+const Modal = ({ contentMessage, heading, setModalDisplay, reqFunction, theme }) => {
+  let closeTheme;
+
+  if (theme == 'error') {
+    closeTheme = 'white';
+  } else {
+    closeTheme = 'purple';
+  }
   return (
     <div className={styles.container}>
       <div className={styles[theme]}>
         <header className={styles.header}>
-          <h2>{heading}</h2>
-          <button className={styles.topCloseButton} onClick={() => setModalDisplay()}>
-            X
-          </button>
+          <h2 className={styles.title}>{heading}</h2>
+          <Button
+            className={styles.topCloseButton}
+            onClick={() => setModalDisplay()}
+            icon={`${process.env.PUBLIC_URL}/assets/images/close-cross-${closeTheme}.svg`}
+          />
         </header>
         <div className={styles.content}>
-          {content ?? null}
-          {contentMessage && !content ? <p className={styles.content}>{contentMessage}</p> : null}
-          {contentMessage && !content ? (
+          {contentMessage ? <p className={styles.content}>{contentMessage}</p> : null}
+          {contentMessage ? (
             <div className={styles.buttons}>
               <Button
                 label={'Cancel'}
                 theme={'primary'}
                 type="submit"
                 onClick={() => {
-                  reqFunction();
+                  setModalDisplay();
                 }}
               />
               <Button
