ab953a5c9d57ce3065ffe0af6b661c1694316fcd
Luciana Prates <lucianaprates10@gmail.com>
2022-11-08 23:19:33 -0300

    TG-29: component table implementation

--- src/Components/Projects/Form/form.js
+++ src/Components/Projects/Form/form.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import styles from '../projects.module.css';
+import Form from '../../Shared/Form/index';
+import Button from '../../Shared/Button';
+import { Input, Select } from '../../Shared/Input/index';
 
 const ProjectsForm = () => {
   const paramsURL = new URLSearchParams(window.location.search);
@@ -24,6 +27,8 @@ const ProjectsForm = () => {
     active: activeValue,
     employees: []
   });
+
+  const roles = ['Rol', 'DEV', 'TL', 'QA', 'PM'];
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/employees`)
@@ -83,14 +88,8 @@ const ProjectsForm = () => {
     }
   };
 
-  const onChangeValue = (key, value, keyArray = false) => {
-    if (keyArray) {
-      setProjectBody({
-        ...projectBody
-      });
-    } else {
-      setProjectBody({ ...projectBody, [key]: value });
-    }
+  const onChange = (e) => {
+    setProjectBody({ ...projectBody, [e.target.name]: e.target.value });
   };
 
   const handleChangeActive = (e) => {
@@ -120,97 +119,135 @@ const ProjectsForm = () => {
     });
   };
 
+  console.log(allEmployees._id);
+
   return (
-    <section className={styles.container}>
-      <form className={styles.container}>
-        <label htmlFor="projectName">Project name:</label>
-        <input
-          id="name"
-          name="name"
-          placeholder="Project name"
-          value={projectBody.name}
-          onChange={(e) => onChangeValue('name', e.target.value)}
+    <>
+      <section className={styles.container}>
+        <form className={styles.container}>
+          <label htmlFor="projectName">Project name:</label>
+          <input
+            id="name"
+            name="name"
+            placeholder="Project name"
+            value={projectBody.name}
+            onChange={onChange}
+          />
+          <label htmlFor="description">Description:</label>
+          <input
+            id="description"
+            name="description"
+            placeholder="Last description"
+            value={projectBody.description}
+            onChange={onChange}
+          />
+          <label htmlFor="startDate">Start Date:</label>
+          <input
+            type="date"
+            id="startDate"
+            name="startDate"
+            placeholder="Start Date"
+            value={projectBody.startDate}
+            onChange={onChange}
+          />
+          <label htmlFor="endDAte">End Date:</label>
+          <input
+            type="date"
+            id="endDAte"
+            name="endDAte"
+            placeholder="End Date"
+            value={projectBody.endDate}
+            onChange={onChange}
+          />
+          <label htmlFor="status">Active:</label>
+          <select name="active" value={projectBody.active} onChange={handleChangeActive}>
+            <option value="false">Inactive</option>
+            <option value="true">Active</option>
+          </select>
+          <label htmlFor="client">Client:</label>
+          <input
+            id="client"
+            name="client"
+            placeholder="Client"
+            value={projectBody.clientName}
+            onChange={onChange}
+          />
+          <div>
+            <h3>Employees:</h3>
+            <select
+              name="id"
+              placeholder="Employee"
+              value={selectedEmployee}
+              onChange={handleChangeEmployee}
+            >
+              <option value="">Employees</option>
+              {allEmployees.map((employee) => {
+                return (
+                  <option key={employee._id} value={employee._id}>
+                    {employee.name} {employee.lastName}
+                  </option>
+                );
+              })}
+            </select>
+            <select name="role" placeholder="Role" value={selectedRole} onChange={handleChangeRole}>
+              <option value="">Role</option>
+              <option value="DEV">DEV</option>
+              <option value="TL">TL</option>
+              <option value="QA">QA</option>
+              <option value="PM">PM</option>
+            </select>
+            <input
+              id="rate"
+              name="rate"
+              placeholder="Rate"
+              value={selectedRate}
+              onChange={handleChangeRate}
+            />
+            <button type="button" onClick={() => assingEmployee()}>
+              Assign
+            </button>
+          </div>
+        </form>
+        <button onClick={sendProject}>Add</button>
+        <a href="/projects">
+          <button>Cancel</button>
+        </a>
+      </section>
+      <Form>
+        <Input title={'ProjectName'} />
+        <Input title={'Description'} />
+        <Input title={'Start Date'} />
+        <Input title={'End Date'} />
+        <Select
+          title={'Active'}
+          arrayToMap={[projectBody.active]}
+          id="active"
+          onChange={onChange}
         />
-        <label htmlFor="description">Description:</label>
-        <input
-          id="description"
-          name="description"
-          placeholder="Last description"
-          value={projectBody.description}
-          onChange={(e) => onChangeValue('description', e.target.value)}
-        />
-        <label htmlFor="startDate">Start Date:</label>
-        <input
-          type="date"
-          id="startDate"
-          name="startDate"
-          placeholder="Start Date"
-          value={projectBody.startDate}
-          onChange={(e) => onChangeValue('startDate', e.target.value)}
-        />
-        <label htmlFor="endDAte">End Date:</label>
-        <input
-          type="date"
-          id="endDAte"
-          name="endDAte"
-          placeholder="End Date"
-          value={projectBody.endDate}
-          onChange={(e) => onChangeValue('endDate', e.target.value)}
-        />
-        <label htmlFor="status">Active:</label>
-        <select name="active" value={projectBody.active} onChange={handleChangeActive}>
-          <option value="false">Inactive</option>
-          <option value="true">Active</option>
-        </select>
-        <label htmlFor="client">Client:</label>
-        <input
-          id="client"
-          name="client"
-          placeholder="Client"
-          value={projectBody.clientName}
-          onChange={(e) => onChangeValue('clientName', e.target.value)}
-        />
+        <Input title={'Client'} />
         <div>
           <h3>Employees:</h3>
-          <select
-            name="id"
-            placeholder="Employee"
-            value={selectedEmployee}
-            onChange={handleChangeEmployee}
-          >
-            <option value="">Employees</option>
-            {allEmployees.map((employee) => {
-              return (
-                <option key={employee._id} value={employee._id}>
-                  {employee.name} {employee.lastName}
-                </option>
-              );
-            })}
-          </select>
-          <select name="role" placeholder="Role" value={selectedRole} onChange={handleChangeRole}>
-            <option value="">Role</option>
-            <option value="DEV">DEV</option>
-            <option value="TL">TL</option>
-            <option value="QA">QA</option>
-            <option value="PM">PM</option>
-          </select>
-          <input
-            id="rate"
-            name="rate"
-            placeholder="Rate"
-            value={selectedRate}
-            onChange={handleChangeRate}
+          <Select
+            arrayToMap={allEmployees.map((employee) => ({
+              id: employee._id,
+              label: employee.name + ' ' + employee.lastName
+            }))}
           />
-          <button type="button" onClick={() => assingEmployee()}>
-            Assign
-          </button>
+          <Select
+            arrayToMap={roles.map((rol) => ({
+              id: rol,
+              label: rol
+            }))}
+          />
         </div>
-      </form>
-      <button onClick={sendProject}>Add</button>
-      <a href="/projects">
-        <button>Cancel</button>
-      </a>
-    </section>
+        <div>
+          <Input placeholder={'Rate'} type={'number'} />
+          <Button label={'Assign'} onClick={assingEmployee} />
+          <Button label={'Add'} />
+          <Button label={'Cancel'} />
+        </div>
+      </Form>
+    </>
   );
 };
 
