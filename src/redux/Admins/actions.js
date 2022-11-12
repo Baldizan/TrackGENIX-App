dda74bbc0f005e341f53c8c641354964f83c6652
Luciana Prates <lucianaprates10@gmail.com>
2022-11-12 14:52:31 -0300

    TG-41: fix on Admins

--- src/redux/Admins/actions.js
+++ src/redux/Admins/actions.js
@@ -1,21 +1,21 @@
-import { GET_TASKS_PENDING, GET_TASKS_SUCCESS, GET_TASKS_ERROR } from './types';
+import { GET_ADMINS_PENDING, GET_ADMINS_SUCCESS, GET_ADMINS_ERROR } from './types';
 
 export const getAdminsPending = () => {
   return {
-    type: GET_TASKS_PENDING
+    type: GET_ADMINS_PENDING
   };
 };
 
 export const getAdminsSuccess = (data) => {
   return {
-    type: GET_TASKS_SUCCESS,
+    type: GET_ADMINS_SUCCESS,
     payload: data
   };
 };
 
 export const getAdminsError = (error) => {
   return {
-    type: GET_TASKS_ERROR,
+    type: GET_ADMINS_ERROR,
     payload: error
   };
 };
