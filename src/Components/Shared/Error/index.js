fd7d350e82af6015b615e6450f58118e3e246cb9
baldizan <marezbaldizan@gmail.com>
2022-11-20 15:01:13 -0300

    TG-78: Implemented dark mode

--- src/Components/Shared/Error/index.js
+++ src/Components/Shared/Error/index.js
@@ -4,8 +4,19 @@ const Error = ({ text }) => {
   return (
     <div className={styles.container}>
       <img className={styles.img} src={`${process.env.PUBLIC_URL}/assets/images/error.svg`} />
-      <h2>Whoops.. It looks like a monster ate your file!</h2>
-      <p className={styles.text}>{text}</p>
+      <div>
+        <h2>Whoops... It looks like a monster ate your file!</h2>
+        <p className={styles.text}>
+          Error&nbsp;code:&nbsp;
+          {text
+            .split('')
+            .reduce((a, b) => {
+              a = (a << 5) - a + b.charCodeAt(0);
+              return a & a;
+            }, 0)
+            .toString(16)}
+        </p>
+      </div>
     </div>
   );
 };
