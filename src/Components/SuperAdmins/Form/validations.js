92d40df452118db87a7f06ffec31998e6f31a68b
elias-92 <martinezelias17@gmail.com>
2022-11-21 02:31:07 -0300

    TG-68:add error messages in validation file

--- src/Components/SuperAdmins/Form/validations.js
+++ src/Components/SuperAdmins/Form/validations.js
@@ -4,14 +4,29 @@ export const schema = Joi.object({
   name: Joi.string()
     .min(3)
     .max(50)
-    .pattern(/^([^0-9]*)$/i, 'Only letters'),
+    .pattern(/^([^0-9]*)$/i)
+    .messages({
+      'string.pattern.base': 'Must contain only letters',
+      'string.min': 'Must have at least 3 characters',
+      'string.max': 'Must contain less than 50 characters'
+    }),
   lastName: Joi.string()
     .min(3)
     .max(50)
-    .pattern(/^([^0-9]*)$/i, 'Only letters'),
-  email: Joi.string().email({ tlds: { allow: false } }),
-  password: Joi.string().pattern(
-    /^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
-    'Letters, numbers and a minimum of 8 characters'
-  )
+    .pattern(/^([^0-9]*)$/i)
+    .messages({
+      'string.pattern.base': 'Must contain only letters',
+      'string.min': 'Must have at least 3 characters',
+      'string.max': 'Must contain less than 50 characters'
+    }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .messages({
+      'string.email': 'Invalid email format.'
+    }),
+  password: Joi.string()
+    .pattern(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)
+    .message({
+      'string.pattern.base': 'It must contain letters, numbers and at least 8 characters.'
+    })
 });
