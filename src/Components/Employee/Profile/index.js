6a508fc36418b290d94aa525fced690068a9354e
Luciana Prates <lucianaprates10@gmail.com>
2022-11-21 17:36:56 -0300

    TG-76: update profile

--- src/Components/Employee/Profile/index.js
+++ src/Components/Employee/Profile/index.js
@@ -1,9 +1,21 @@
+import Form from 'Components/Shared/Form';
+import { Input } from 'Components/Shared/Input';
 import styles from './profile.module.css';
 
 const EmployeeProfile = () => {
   return (
     <section className={styles.container}>
-      <h2>Profile</h2>
+      <Form title="My profile">
+        <Input placeholder="Edit your user name" name="userName" title="User name" required />
+        <Input placeholder="Edit your email" name="mail" title="Mail" required />
+        <Input placeholder="Edit your password" name="password" title="Password" required />
+        <Input
+          placeholder="Repeat password"
+          name="repeatPassword"
+          title="Repeat password"
+          required
+        />
+      </Form>
     </section>
   );
 };
