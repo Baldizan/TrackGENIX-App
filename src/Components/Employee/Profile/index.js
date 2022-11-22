0b3d44ee9bdcad9af7d1bae91304d96fc7912c1a
Luciana Prates <lucianaprates10@gmail.com>
2022-11-22 09:22:52 -0300

    TG-76: update form

--- src/Components/Employee/Profile/index.js
+++ src/Components/Employee/Profile/index.js
@@ -1,20 +1,55 @@
 import Form from 'Components/Shared/Form';
 import { Input } from 'Components/Shared/Input';
 import styles from './profile.module.css';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { putEmployee } from 'redux/Employees/thunks';
 
 const EmployeeProfile = () => {
+  const dispatch = useDispatch();
+  const EmployeeId = '636e639dfb8b4c835d213750';
+  const EmployeeProfile = {
+    userName: 'Pedro',
+    lastName: 'Diazz',
+    phone: 1231231230,
+    email: 'test@radium.com',
+    password: '2022BaSP'
+  };
+
+  const { handleSubmit, register, reset } = useForm({
+    mode: 'all'
+  });
+
+  useEffect(() => {
+    if (EmployeeProfile) {
+      reset(EmployeeProfile);
+    }
+  }, []);
+
+  const onSubmit = (data) => {
+    dispatch(putEmployee(EmployeeId, data));
+  };
+
   return (
     <section className={styles.container}>
-      <Form title="My profile">
-        <Input placeholder="Edit your user name" name="userName" title="User name" required />
-        <Input placeholder="Edit your email" name="mail" title="Mail" required />
-        <Input placeholder="Edit your password" name="password" title="Password" required />
+      <Form title="My profile" onSubmit={handleSubmit(onSubmit)}>
+        <Input placeholder="Edit your name" name="name" title="Name" register={register} />
         <Input
-          placeholder="Repeat password"
-          name="repeatPassword"
-          title="Repeat password"
-          required
+          placeholder="Edit your last name"
+          name="lastName"
+          title="User name"
+          register={register}
         />
+        <Input placeholder="Edit your phone" name="phone" title="Phone" register={register} />
+        <Input placeholder="Edit your email" name="mail" title="Mail" register={register} />
+        <Input
+          placeholder="Edit your password"
+          name="password"
+          title="Password"
+          register={register}
+        />
+        <Input placeholder="Repeat password" name="repeatPassword" title="Repeat password" />
       </Form>
     </section>
   );
