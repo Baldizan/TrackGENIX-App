397fd8c11d189bb48b52ed000452800760d53f10
Guido Gentile <guido.gentile.1995@gmail.com>
2022-11-22 01:48:38 -0300

    TG-74: Made employees projects page

--- src/Components/Employee/Projects/index.js
+++ src/Components/Employee/Projects/index.js
@@ -1,9 +1,49 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProjects } from 'redux/Projects/thunks';
 import styles from './projects.module.css';
+import Table from 'Components/Shared/Table';
+import Loader from 'Components/Shared/Loader';
+import Error from 'Components/Shared/Error';
 
 const EmployeeProjects = () => {
+  const { list: projectsList, isPending, error } = useSelector((state) => state.projects);
+  const dispatch = useDispatch();
+  const headers = {
+    name: 'Project Name',
+    description: 'Description',
+    clientName: 'Client Name',
+    startDate: 'Start Date',
+    endDate: 'End Date'
+  };
+
+  useEffect(() => {
+    dispatch(getProjects());
+  }, []);
+
+  const mockedEmployeeLogged = '63756ccf76cb20ad5433fa22';
+  const projectsData = () => {
+    let employeeProjects = [];
+    for (let i = 0; i < projectsList.length; i++) {
+      for (let j = 0; j < projectsList[i].employees.length; j++) {
+        if (projectsList[i].employees[j].id._id === mockedEmployeeLogged) {
+          projectsList[i].startDate = projectsList[i].startDate.slice(0, 10);
+          projectsList[i].endDate = projectsList[i].endDate.slice(0, 10);
+          employeeProjects.push(projectsList[i]);
+        }
+      }
+    }
+    return employeeProjects;
+  };
+
   return (
     <section className={styles.container}>
-      <h2>Projects</h2>
+      {isPending && <Loader />}
+      {error && <Error text={error} />}
+      {!isPending && !error && (
+        <Table headers={headers} data={projectsData()} title="My projects" itemsPerPage={5} />
+      )}
+      {error && <p>{error}</p>}
     </section>
   );
 };
