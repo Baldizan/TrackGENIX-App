62f7a4bd11e94795b444917c8b8952bf1073a626
elias-92 <martinezelias17@gmail.com>
2022-11-23 12:45:48 -0300

    TG-71:add validation in input

--- src/Components/Tasks/TasksForm/validations.js
+++ src/Components/Tasks/TasksForm/validations.js
@@ -1,9 +1,15 @@
 import Joi from 'joi';
 
 export const schema = Joi.object({
-  description: Joi.string().min(5).max(50).required().messages({
-    'string.min': 'Description must contain at least 5 letters.',
-    'string.max': 'Description cannot have over 50 letters.',
-    'string.empty': 'Description is required'
-  })
+  description: Joi.string()
+    .min(5)
+    .max(50)
+    .pattern(/^(?=.[a-zA-Z].)([\w\s\W]+)$/)
+    .required()
+    .messages({
+      'string.min': 'Description must contain at least 5 letters.',
+      'string.max': 'Description cannot have over 50 letters.',
+      'string.empty': 'Description is required',
+      'string.pattern.base': `Description can't have only numbers or special characters`
+    })
 });
