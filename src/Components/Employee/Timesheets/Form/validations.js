a0bcbfcf4a3071d30d8f2a92c64be15c4f6a59de
baldizan <marezbaldizan@gmail.com>
2022-11-23 17:21:06 -0300

    TG-75: Correction based on comments

--- src/Components/Employee/Timesheets/Form/validations.js
+++ src/Components/Employee/Timesheets/Form/validations.js
@@ -3,6 +3,7 @@ import Joi from 'joi';
 export const schema = Joi.object({
   date: Joi.date(),
   hours: Joi.number().min(0).messages({
+    'number.base': 'Hours must be a number',
     'number.min': 'You cannot subtract hours.'
   })
 });
