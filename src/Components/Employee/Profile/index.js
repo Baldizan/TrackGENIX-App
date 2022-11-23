ef1c9a4f0d0d0dc8af3ec714f062a37f0cce617e
Luciana Prates <lucianaprates10@gmail.com>
2022-11-23 18:55:35 -0300

    TG-76: add validations

--- src/Components/Employee/Profile/index.js
+++ src/Components/Employee/Profile/index.js
@@ -1,24 +1,34 @@
-import Form from 'Components/Shared/Form';
-import { Input } from 'Components/Shared/Input';
-import styles from './profile.module.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { schema } from './validations';
+import styles from './profile.module.css';
 import { putEmployee } from 'redux/Employees/thunks';
+import Form from 'Components/Shared/Form';
+import { Input } from 'Components/Shared/Input';
+import Modal from 'Components/Shared/Modal';
 
 const EmployeeProfile = () => {
   const dispatch = useDispatch();
   const EmployeeId = '636e639dfb8b4c835d213750';
+  const [isModal, setIsModal] = useState(false);
   const EmployeeProfile = {
     name: 'Pedro',
     lastName: 'Diazz',
-    phone: 1231231230,
-    mail: 'test@radium.com',
-    password: '2022BaSP'
+    phone: '1234578911',
+    email: 'test@radium.com',
+    password: '2022BaSP',
+    repeatPassword: '2022BaSP'
   };
-
-  const { handleSubmit, register, reset } = useForm({
-    mode: 'all'
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors, isValid }
+  } = useForm({
+    mode: 'all',
+    resolver: joiResolver(schema)
   });
 
   useEffect(() => {
@@ -29,24 +39,27 @@ const EmployeeProfile = () => {
 
   const onSubmit = (data) => {
     dispatch(putEmployee(EmployeeId, data));
+    setIsModal(true);
   };
 
   return (
     <section className={styles.container}>
-      <Form title="My profile" onSubmit={handleSubmit(onSubmit)}>
+      <Form title="My profile" onSubmit={handleSubmit(onSubmit)} noValidate={!isValid}>
         <Input
           placeholder="Edit your name"
           id="name"
           name="name"
           title="Name"
           register={register}
+          error={errors.name?.message}
         />
         <Input
           placeholder="Edit your last name"
           id="lastName"
           name="lastName"
-          title="User name"
+          title="Last Name"
           register={register}
+          error={errors.lastName?.message}
         />
         <Input
           placeholder="Edit your phone"
@@ -54,13 +67,15 @@ const EmployeeProfile = () => {
           name="phone"
           title="Phone"
           register={register}
+          error={errors.phone?.message}
         />
         <Input
           placeholder="Edit your email"
-          id="mail"
-          name="mail"
-          title="Mail"
+          id="email"
+          name="email"
+          title="Email"
           register={register}
+          error={errors.mail?.message}
         />
         <Input
           placeholder="Edit your password"
@@ -69,6 +84,7 @@ const EmployeeProfile = () => {
           title="Password"
           type="password"
           register={register}
+          error={errors.password?.message}
         />
         <Input
           placeholder="Repeat password"
@@ -77,7 +93,11 @@ const EmployeeProfile = () => {
           title="Repeat password"
           type="password"
           register={register}
+          error={errors.repeatPassword?.message}
         />
+        {isModal && (
+          <Modal heading="user edited successfully" theme="success" setModalDisplay={setIsModal} />
+        )}
       </Form>
     </section>
   );
---
+++ src/Components/Employee/Profile/validations.js
@@ -0,0 +1,46 @@
+import Joi from 'joi';
+
+export const schema = Joi.object({
+  name: Joi.string()
+    .regex(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/)
+    .min(3)
+    .max(20)
+    .messages({
+      'string.empty': 'First Name is required.',
+      'string.pattern.base': 'Name should be letters only',
+      'string.min': 'First name must contain at least 3 letters.',
+      'string.max': 'First name cannot have over 40 letters.'
+    }),
+  lastName: Joi.string()
+    .regex(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/)
+    .min(3)
+    .max(20)
+    .messages({
+      'string.empty': 'Last Name is required.',
+      'string.pattern.base': 'Name should be letters only',
+      'string.min': 'Last name must contain at least 3 letters.',
+      'string.max': 'Last name cannot have over 40 letters.'
+    }),
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .message('Phone number must be a 10 digits value.'),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .lowercase()
+    .messages({
+      'string.empty': 'Email is required.',
+      'string.email': 'Invalid email format.'
+    }),
+  password: Joi.string()
+    .min(8)
+    .regex(/^(?=.*?[a-zA-Z])(?=.*?[0-9])/)
+    .messages({
+      'string.empty': 'Password is required.',
+      'string.min': 'Password must contain at least 8 characters.',
+      'string.pattern.base': 'Password must contain both letters and numbers.'
+    }),
+  repeatPassword: Joi.any().valid(Joi.ref('password')).required().messages({
+    'any.only': 'The password has to be the same',
+    'any.required': 'Repeat Password is required'
+  })
+});
