00d6bf1f8975515fc6f9b8c39f8dff5356844618
elias-92 <martinezelias17@gmail.com>
2022-11-25 11:57:30 -0300

    TG-71:correct validation in description field

--- src/Components/Tasks/TasksForm/validations.js
+++ src/Components/Tasks/TasksForm/validations.js
@@ -4,12 +4,12 @@ export const schema = Joi.object({
   description: Joi.string()
     .min(5)
     .max(50)
-    .pattern(/^(?=.[a-zA-Z].)([\w\s\W]+)$/)
+    .pattern(/[A-Za-z]{5}([A-Za-z]+ ?)*/)
     .required()
     .messages({
       'string.min': 'Description must contain at least 5 letters.',
       'string.max': 'Description cannot have over 50 letters.',
       'string.empty': 'Description is required',
-      'string.pattern.base': `Description can't have only numbers or special characters`
+      'string.pattern.base': 'Description must have a minimum of 5 letters'
     })
 });
